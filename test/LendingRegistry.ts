c1e31222033a7a6439f034901ebda9314c4bf70a
Fraser <rootminus0x1@gmail.com>
2023-07-09 22:34:41 +0100

    debug typescript tests

--- test/LendingRegistry.ts
+++ test/LendingRegistry.ts
@@ -6,6 +6,9 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+const zeroAddress = '0x0000000000000000000000000000000000000000';
+const zeroBytes32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
 describe("LendingRegistry", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
@@ -24,13 +27,18 @@ describe("LendingRegistry", function () {
     it("Should not fail", async function () {
       const { lendingRegistry, owner, otherAccount } = await loadFixture(deployFixture);
 
-      // expect no errors;
+      const ownerAddress = await lendingRegistry.owner();
+      expect(ownerAddress).to.equal(owner.address);
+      expect(ownerAddress).to.equal('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
+
+      const bestApr = await lendingRegistry.getBestApr(zeroAddress, []);
+      
+      expect(bestApr[0]).to.equal(0.0);
+      expect(bestApr[1]).to.equal(zeroBytes32);
+
+      expect(bestApr.apr).to.equal(0.0);
+      expect(bestApr.protocol).to.equal(zeroBytes32);
     });
 
-    it("Should set the right owner", async function () {
-      const { lendingRegistry, owner } = await loadFixture(deployFixture);
-
-      expect(await lendingRegistry.owner()).to.equal(owner.address);
-    });
   })
 });
